1d5dcdec5004e1f79c4c26a115227d1e992eef17
manolobkno08 <manologm10@hotmail.com>
2021-12-21 00:07:03 -0500

    permissions

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,6 @@
 -- MySQL server for the project
 -- because Batch 3 is the best!
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev' @'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON 'hbnb_dev_db'.* TO 'hbnb_dev' @'localhost';
-GRANT
-SELECT
-  ON 'performance_schema'.* TO 'hbnb_dev' @'localhost';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON 'hbnb_dev_db'.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON 'performance_schema'.* TO 'hbnb_dev'@'localhost';
