16825ce842c24e28ba6103fd22e8ff704e7ed4c6
manolobkno08 <manologm10@hotmail.com>
2022-01-13 18:56:28 -0500

    fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,35 +1,30 @@
 #!/usr/bin/python3
-
 """
-Files deployment
-
+Deploying tgz file to our servers
 """
-from fabric.api import env, put, run
+
+from fabric.api import put, run, env
 from os.path import exists
-
 env.hosts = ['35.237.25.66', '34.234.71.240']
 
 
 def do_deploy(archive_path):
-    """ Deploy files """
+    """deploy web static with fabric"""
     if exists(archive_path) is False:
         return False
-    try:
-        filename = archive_path.split('/')[-1]
-        no_ext = filename.split('.')[0]
-        final_path = "/data/web_static/releases/"
 
+    try:
+        filename = archive_path.split("/")[-1]
+        no_excep = filename.split(".")[0]
+        path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('sudo mkdir -p {}{}/'.format(final_path, no_ext))
-        run('sudo tar -xzf /tmp/{} -C {}{}/'.format(
-            filename, final_path, no_ext))
+        run('sudo mkdir -p {}{}/'.format(path, no_excep))
+        run('sudo tar -xzf /tmp/{} -C {}{}/'.format(filename, path, no_excep))
         run('sudo rm /tmp/{}'.format(filename))
-        run('sudo mv {0}{1}/web_static/* {0}{1}/'.format(
-            final_path, no_ext))
-        run('sudo rm -rf {}{}/web_static'.format(final_path, no_ext))
+        run('sudo mv {0}{1}/web_static/* {0}{1}/'.format(path, no_excep))
+        run('sudo rm -rf {}{}/web_static'.format(path, no_excep))
         run('sudo rm -rf /data/web_static/current')
-        run('sudo ln -s {}{}/ /data/web_static/current'.format(
-            final_path, no_ext))
+        run('sudo ln -s {}{}/ /data/web_static/current'.format(path, no_excep))
         return True
     except BaseException:
         return False
