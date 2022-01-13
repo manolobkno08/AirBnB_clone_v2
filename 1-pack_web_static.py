2c7b2eab7e20c3778e79894c04222774e3c2b38f
manolobkno08 <manologm10@hotmail.com>
2022-01-13 01:56:08 -0500

    add task1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from fabric.api import local
+from os.path import isdir
+import datetime
+
+date_now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+
+
+def do_pack():
+    try:
+        if isdir('versions') is False:
+            local("mkdir versions")
+        filename = "versions/web_static_{}.tgz".format(date_now)
+        local('tar -cvzf {} web_static'.format(filename))
+        return filename
+    except Exception:
+        return None
