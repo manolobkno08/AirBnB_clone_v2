6fac9e3b92f7f1d1b2b2067d26e665002b674f5b
manolobkno08 <manologm10@hotmail.com>
2022-01-13 17:04:16 -0500

    task2 fixed

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,10 +7,7 @@ Files deployment
 from fabric.api import env, put, run
 from os.path import exists
 
-env.hosts = [
-    '35.237.25.66',
-    '34.234.71.240'
-]
+env.hosts = ['35.237.25.66', '34.234.71.240']
 env.user = "ubuntu"
 
 
@@ -23,22 +20,15 @@ def do_deploy(archive_path):
         no_ext = archive_path.split('/')[1].split('.')[0]
         final_path = "/data/web_static/releases/"
 
-        # Upload file into the tmp directory
         put('{}, /tmp/'.format(archive_path))
-        # Create a folder into the server
         run('sudo mkdir -p {}{}/'.format(final_path, no_ext))
-        # Uncompress file
         run('sudo tar -xzf /tmp/{} -C {}{}/'.format(
             filename, final_path, no_ext))
-        # Delete tgz file
         run('sudo rm /tmp/{}'.format(filename))
-        # Move
         run('sudo mv {0}{1}/web_static/* {0}{1}/'.format(
             final_path, no_ext))
         run('sudo rm -rf {}{}/web_static'.format(final_path, no_ext))
-        # Delete current simbolic link
         run('sudo rm -rf /data/web_static/current')
-        # Create new simbolic link
         run('sudo ln -s {}{}/ /data/web_static/current'.format(
             final_path, no_ext))
         return True
