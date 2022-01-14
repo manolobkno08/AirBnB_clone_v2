67143e0a375264b717cf752906f1e06923ff4809
manolobkno08 <manologm10@hotmail.com>
2022-01-13 19:17:56 -0500

    task2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,6 +6,7 @@ Deploying tgz file to our servers
 from fabric.api import put, run, env
 from os.path import exists
 env.hosts = ['35.237.25.66', '34.234.71.240']
+env.user = 'ubuntu'
 
 
 def do_deploy(archive_path):
@@ -18,13 +19,13 @@ def do_deploy(archive_path):
         no_excep = filename.split(".")[0]
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('sudo mkdir -p {}{}/'.format(path, no_excep))
+        run('mkdir -p {}{}/'.format(path, no_excep))
         run('sudo tar -xzf /tmp/{} -C {}{}/'.format(filename, path, no_excep))
-        run('sudo rm /tmp/{}'.format(filename))
+        run('rm /tmp/{}'.format(filename))
         run('sudo mv {0}{1}/web_static/* {0}{1}/'.format(path, no_excep))
-        run('sudo rm -rf {}{}/web_static'.format(path, no_excep))
-        run('sudo rm -rf /data/web_static/current')
-        run('sudo ln -s {}{}/ /data/web_static/current'.format(path, no_excep))
+        run('rm -rf {}{}/web_static'.format(path, no_excep))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_excep))
         return True
-    except BaseException:
+    except Exception:
         return False
