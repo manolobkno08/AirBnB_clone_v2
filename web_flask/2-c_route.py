95408de6f7c3be8f4fa5283ae0c9d2aaac9069e5
manolobkno08 <manologm10@hotmail.com>
2022-01-29 11:14:52 -0500

    delete cache and push task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,21 +1,28 @@
 #!/usr/bin/python3
 
+"""
+starts a Flask web application
+"""
+
 from flask import Flask, escape
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """returns Hello HBNB!"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """returns HBNB"""
     return "HBNB!"
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def C_is_fun(text):
+    """returns a parameter by url as string"""
     txt = text.replace('_', ' ')
     return "C {}".format(escape(txt))
 
