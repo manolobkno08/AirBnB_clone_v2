db1c4c5bf134acedf230bdff18eed7ed8525b9cb
manolobkno08 <manologm10@hotmail.com>
2022-01-29 12:18:19 -0500

    add task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python3
+"""
+starts a Flask web application
+"""
 
-from flask import Flask, escape
+from flask import Flask, escape, render_template
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """returns Hello HBNB!"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """returns HBNB"""
     return "HBNB!"
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def C_is_fun(text):
+    """returns a parameter by url as string"""
     txt = text.replace('_', ' ')
     return "C {}".format(escape(txt))
 
@@ -23,14 +29,22 @@ def C_is_fun(text):
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def Py_is_fun(text):
+    """Set a parameter by or return passed value"""
     txt = text.replace('_', ' ')
     return "Python {}".format(escape(txt))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def n_is_num(n):
+    """Check if a parameter is integer type and return"""
     if isinstance(n, int):
         return "{} is a number".format(escape(n))
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def render_n_to_template(n):
+    """Check if a parameter is int and return by template"""
+    return render_template("5-number.html", n=n)
 
 
 if __name__ == '__main__':
