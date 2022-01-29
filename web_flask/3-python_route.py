efe801ff6614fdc51d6cb07172e5d702ecc85778
manolobkno08 <manologm10@hotmail.com>
2022-01-29 11:17:58 -0500

    task 3 commented

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+starts a Flask web application
+"""
 
 from flask import Flask, escape
 app = Flask(__name__)
@@ -6,16 +9,19 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """returns Hello HBNB!"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """returns HBNB"""
     return "HBNB!"
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def C_is_fun(text):
+    """returns a parameter by url as string"""
     txt = text.replace('_', ' ')
     return "C {}".format(escape(txt))
 
@@ -23,6 +29,7 @@ def C_is_fun(text):
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def Py_is_fun(text):
+    """Set a parameter by or return passed value"""
     txt = text.replace('_', ' ')
     return "Python {}".format(escape(txt))
 
