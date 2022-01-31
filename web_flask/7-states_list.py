97703c14846a7713d4b7430202b72a9b72eafd1c
manolobkno08 <manologm10@hotmail.com>
2022-01-31 01:27:30 -0500

    task 8 fixed

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,6 +5,7 @@ Fetch data by Flask
 """
 
 from flask import Flask, render_template
+from models.state import State
 from models import storage
 
 app = Flask(__name__)
@@ -13,8 +14,8 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def fetch_all_states():
     """Get states from DBStorage"""
-    fetch_states = storage.all("State").values()
-    return render_template("7-states_list.html", states=fetch_states)
+    states = storage.all(State).values()
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
@@ -24,4 +25,4 @@ def teardown_db(self):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
